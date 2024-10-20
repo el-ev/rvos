4ca746f10e095495b6504877709798a99ed9d2da
Qin-shihuang <0.0@owo.li>
2024-10-20 13:35:40 +0800

    feat: support c-style user string

--- kernel/src/utils/user_string.rs
+++ kernel/src/utils/user_string.rs
@@ -2,41 +2,88 @@ use core::fmt::{self, Display};
 
 use alloc::sync::Arc;
 
-use crate::{mm::{addr::VirtAddr, address_space::U_END}, task::{taskdef::TaskControlBlock, user_space::UserAreaPerm}};
+use crate::{
+    mm::{addr::VirtAddr, address_space::U_END, consts::PAGE_SIZE},
+    task::{taskdef::TaskControlBlock, user_space::UserAreaPerm},
+};
 
 #[derive(Clone)]
 pub struct UnsafeUserString {
     task: Arc<TaskControlBlock>,
     ptr: *const u8,
-    len: usize,
+    len: Option<usize>,
 }
 
 impl UnsafeUserString {
-    pub fn new(task: Arc<TaskControlBlock>, ptr: *const u8, len: usize) -> Self {
+    pub fn new(task: Arc<TaskControlBlock>, ptr: *const u8, len: Option<usize>) -> Self {
         Self { task, ptr, len }
     }
 
-    pub unsafe fn as_str(&self) -> &str {
-        unsafe { core::str::from_utf8_unchecked(core::slice::from_raw_parts(self.ptr, self.len)) }
+    unsafe fn as_str(&self) -> &str {
+        unsafe { core::str::from_utf8_unchecked(core::slice::from_raw_parts(self.ptr, self.len.unwrap())) }
     }
 
     pub fn checked(&self) -> Option<UserString> {
-        if self.ptr as usize >= U_END || (self.ptr as usize).checked_add(self.len).is_none()  || (self.ptr as usize + self.len) >= U_END {
-            return None;
-        }
-        let mut vpn = VirtAddr(self.ptr as usize).floor_page();
-        let end = VirtAddr(self.ptr as usize + self.len).ceil_page();
-        while vpn < end {
-            // let pte = self.task.page_table().query(vpn);
-            // if pte.is_none() || !pte.unwrap().flags().contains(PteFlags::U | PteFlags::V | PteFlags::R) {
-            //     return None;
-            // }
-            if !self.task.memory().lock().check_perm(vpn, UserAreaPerm::R) {
+        if self.len.is_none() {
+            if self.ptr as usize >= U_END {
                 return None;
             }
-            vpn += 1;
+            let mut ptr = self.ptr as usize;
+            let mut len = 0;
+
+            loop {
+                if ptr >= U_END {
+                    return None;
+                }
+
+                let vpn = VirtAddr(ptr).floor_page();
+                if !self.task.memory().lock().check_perm(vpn, UserAreaPerm::R) {
+                    return None;
+                }
+                while ptr % PAGE_SIZE != 0 || ptr >= U_END {
+                    if unsafe { *(ptr as *const u8) } == 0 {
+                        break;
+                    }
+
+                    ptr += 1;
+                    len += 1;
+                }
+                if unsafe { *(ptr as *const u8) } == 0 {
+                    break;
+                }
+                ptr += 1;
+                len += 1;
+            }
+            Some(UserString {
+                inner: UnsafeUserString {
+                    task: self.task.clone(),
+                    ptr: self.ptr,
+                    len: Some(len),
+                },
+            })
+        } else {
+            if self.ptr as usize >= U_END
+                || (self.ptr as usize).checked_add(self.len.unwrap()).is_none()
+                || (self.ptr as usize + self.len.unwrap()) >= U_END
+            {
+                return None;
+            }
+            let mut vpn = VirtAddr(self.ptr as usize).floor_page();
+            let end = VirtAddr(self.ptr as usize + self.len.unwrap()).ceil_page();
+            while vpn < end {
+                // let pte = self.task.page_table().query(vpn);
+                // if pte.is_none() || !pte.unwrap().flags().contains(PteFlags::U | PteFlags::V | PteFlags::R) {
+                //     return None;
+                // }
+                if !self.task.memory().lock().check_perm(vpn, UserAreaPerm::R) {
+                    return None;
+                }
+                vpn += 1;
+            }
+            Some(UserString {
+                inner: self.clone(),
+            })
         }
-        Some(UserString { inner: self.clone() })
     }
 }
 
