adc748cbd1f20cb871735b00c4cbd66a7ec7e373
Qin-shihuang <0.0@owo.li>
2024-10-28 10:38:01 +0800

    refactor: optimized HartLocal

--- kernel/src/task/hart.rs
+++ kernel/src/task/hart.rs
@@ -4,47 +4,39 @@ use alloc::sync::Arc;
 use arch::tp;
 use sync::Lazy;
 
-use crate::{Mutex, config::CPU_NUM};
+use crate::config::CPU_NUM;
 
 use super::taskdef::TaskControlBlock;
 
-pub struct HartLocal {
-    #[allow(dead_code)]
-    pub hart_id: usize,
-    pub ipi_pending: AtomicBool,
-    pub current_task: Option<Arc<TaskControlBlock>>,
+// Guaranteed to be only accessed by the corresponding hart
+struct HartLocal {
+    ipi_pending: AtomicBool,
+    current_task: Option<Arc<TaskControlBlock>>,
 }
 
-static mut HART_LOCAL: Lazy<[Mutex<HartLocal>; CPU_NUM]> = Lazy::new(|| {
+static mut HART_LOCAL: Lazy<[HartLocal; CPU_NUM]> = Lazy::new(|| {
     let mut array = [const { MaybeUninit::uninit() }; CPU_NUM];
-    for (i, elem) in array.iter_mut().enumerate() {
-        *elem = MaybeUninit::new(Mutex::new(HartLocal {
-            hart_id: i,
+    for elem in array.iter_mut() {
+        *elem = MaybeUninit::new(HartLocal {
             ipi_pending: AtomicBool::new(false),
             current_task: None,
-        }));
+        });
     }
-    unsafe { core::mem::transmute::<_, [Mutex<HartLocal>; CPU_NUM]>(array) }
+    unsafe { core::mem::transmute::<_, [HartLocal; CPU_NUM]>(array) }
 });
 
-#[inline(always)]
-pub fn get_hart_id() -> usize {
-    tp()
-}
-
 pub fn get_current_task() -> Option<Arc<TaskControlBlock>> {
-    let hart_id = get_hart_id();
-    unsafe { &HART_LOCAL[hart_id] }.lock().current_task.clone()
+    let hart_id = tp();
+    unsafe { &HART_LOCAL[hart_id] }.current_task.clone()
 }
 
 pub fn set_current_task(task: Option<Arc<TaskControlBlock>>) {
-    let hart_id = get_hart_id();
-    unsafe { &HART_LOCAL[hart_id] }.lock().current_task = task;
+    let hart_id = tp();
+    unsafe { &mut HART_LOCAL[hart_id] }.current_task = task;
 }
 
 pub fn wake_hart(hart_id: usize) {
     unsafe { &HART_LOCAL[hart_id] }
-        .lock()
         .ipi_pending
         .store(true, core::sync::atomic::Ordering::Release);
     sbi::legacy::sbi_send_ipi(1 << hart_id);
@@ -54,9 +46,8 @@ pub fn clear_ipi() {
     unsafe {
         riscv::register::sip::clear_ssoft();
     }
-    let hart_id = get_hart_id();
+    let hart_id = tp();
     unsafe { &HART_LOCAL[hart_id] }
-        .lock()
         .ipi_pending
         .store(false, core::sync::atomic::Ordering::Release);
 }
